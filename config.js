1a69490cb828a819caf36bed40cfea464c0f570a
DinhHieu7789 <85294018+DinhHieu7789@users.noreply.github.com>
2021-06-03 21:01:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const CONFIG = {
     desc: '😊😊😊',
     btnYes: 'Yêu anh siêu nhiều luôn 😘 ',
     btnNo: 'Yêu anh vừa vừa😂',
-    question:'Anh hỏi em nhá.Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Anh hỏi em nhá.Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh ❤',
     btnReply: 'Gửi cho anh yêu ❤',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Anh cũng yêu em nhiều nhiều lắmmm 💋💋💋',
