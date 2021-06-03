4bcaca5dd7154442f991dae012ef0b2b0a06f2b6
DinhHieu7789 <85294018+DinhHieu7789@users.noreply.github.com>
2021-06-03 20:45:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,12 +6,11 @@ const CONFIG = {
     desc: 'Em Yêu Anh Hônggg ??? ',
     btnYes: 'Yêu anh siêu nhiều luôn 😘 ',
     btnNo: 'Yêu anh vừa vừa',
-    question:'Anh hỏi em nhá',
-    'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Anh hỏi em nhá.Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh yêu ❤',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Anh cũng yêu em nhiều nhiều lắmmm 💋💋💋',
     messDesc: 'Tối mai 8h anh xuống chơi với em yêu nhá',
-    btnAccept: 'Vâng ạ,
+    btnAccept: 'Vâng ạ',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
